e3a197391d0cebd77144bd31537efca7483bfdd9
jeff chen <timercrack@gmail.com>
2016-01-04 00:02:24 +0800

    add darwin log file

--- pydatacoll/utils/logger.py
+++ pydatacoll/utils/logger.py
@@ -1,20 +1,22 @@
 import logging
 import threading
-# import platform
+import platform
 
 initLock = threading.Lock()
 rootLoggerInitialized = False
 
 log_format = "%(asctime)s %(name)s [%(levelname)s] %(message)s"
 level = logging.ERROR
-file_log = "pydatacoll.log"  # File name
+file_log = None  # File name
 console_log = True
-# sys_str = platform.system()
-# if sys_str == "Windows":
-#     file_log = "pydatacoll.log"
-# elif sys_str == "Linux":
-#     file_log = "pydatacoll.log"
-#     console_log = False
+sys_str = platform.system()
+if sys_str == "Windows":
+    file_log = "d:/pydatacoll.log"
+elif sys_str == "Linux":
+    file_log = "/var/log/pydatacoll.log"
+    console_log = False
+elif sys_str == "Darwin":  # Mac OS X
+    file_log = "~/Documents/pydatacoll.log"
 
 
 def init_handler(handler):
